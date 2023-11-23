4919b5cefcb5a46e2b54aa199f91af617ce8a3d5
Pacharatnkp <pachara.pplzz@gmail.com>
2023-11-23 17:46:17 +0700

    Js-Ex1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalAmount = 27000;
+let discountPercen = 0.25; // 25/100
+let discountedAmount = totalAmount * discountPercen;
+let finalAmount = totalAmount - discountedAmount;
+
--- ex-6.js
+++ ex-6.js
@@ -2,5 +2,5 @@
 
 let totalParts = 13000;
 
-let concatSentence;
-let literalSentence;
+let literalSentence = `Total: ${totalParts} Machine parts`;
+
